612c92f70c289616213b47b8af30bfb10c99b27e
Nguyen Ngoc Thang <nguyenngocthang0939@gmail.com>
2024-04-10 17:48:55 +0700

    push API crawling porjects

---
+++ weather API/extract.py
@@ -0,0 +1,25 @@
+# import requests
+# import datetime as dt
+
+# BASE_URL = "http://api.openweathermap.org/data/2.5/weather?"
+# API_KEY = open('api_key.txt', 'r').read()
+
+# CITY = "New York"
+
+# url = f'{BASE_URL}q={CITY}&appid={API_KEY}'
+# response = requests.get(url).json()
+
+
+# sunrise_time = dt.datetime.utcfromtimestamp(response['sys']['sunrise'] + response['timezone'])
+# sunset_time = dt.datetime.utcfromtimestamp(response['sys']['sunset'] + response['timezone'])
+# country_name = response['sys']['country']
+
+
+# print(f'{CITY} is a city in {country_name}')
+# print(f'sunrise time in {CITY} is {sunrise_time}')
+# print(f'sunset time in {CITY} is {sunset_time}')
+
+tk = (8 + 9 + 8) / 3
+gk = 0.5
+ck = 6
+print(5 * 0.2 + gk * 0.3 + ck * 0.5)
